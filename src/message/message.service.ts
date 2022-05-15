af544df0bc1764729c8438fcb5800510779a64af
19522264 <19522264@gm.uit.edu.vn>
2022-05-15 19:57:17 +0700

    hmm

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -5,12 +5,31 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class MessageService {
     constructor(private prismaService: PrismaService) {}
     async getLastestMessages(email : string){
-        return await this.prismaService.lastestmessage.findMany({
+        const mess = await this.prismaService.lastestmessage.findMany({
             where: {
                 participants: {
                     has: email
                 }
             }
         })
+        let result = []
+        for (const index of mess){
+            const fremail = index.participants.find((e) => e !== email)
+            const res = await this.prismaService.userprofile.findUnique({
+                where: {
+                    email: fremail
+                },
+                select:{
+                    photoURL: true,
+                    displayName: true
+                }
+            })
+            result.push({
+                ...index,
+                partnerPhotoURL: res.photoURL,
+                partnerName: res.displayName
+            })
+        }
+        return result
     }
 }
