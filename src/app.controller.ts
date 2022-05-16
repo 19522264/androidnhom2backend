49b0fd169a64822fcde00fad5ef6e1e9025a1baa
19522264 <19522264@gm.uit.edu.vn>
2022-05-16 17:45:44 +0700

    hmm

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,16 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
+  constructor(private readonly prismaService: PrismaService){}
   @Get()
   gethello(){
       return "Henoa"
+  }
+  @Get('school')
+  async getSchool(){
+    return await this.prismaService.school.findMany({select: {label: true, value: true}})
   }
 }
